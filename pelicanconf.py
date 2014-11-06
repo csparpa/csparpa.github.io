d36500930b9b5ae127ab6b04e0b3c2c184e03edd
csparpa <csparpa@gmail.com>
2014-11-06 10:18:20 +0000

    Add CNAME file for DNS settings

--- pelicanconf.py
+++ pelicanconf.py
@@ -2,45 +2,47 @@
 # -*- coding: utf-8 -*- #
 from __future__ import unicode_literals
 
+# Blog global info
 AUTHOR = u'csparpa'
 SITENAME = u'Vorsprung durch Informatik'
 SITEURL = 'http://csparpa.github.io'
 
+# Path for source .md files
 PATH = 'content'
 
+# Date/language settings
 TIMEZONE = 'Europe/Rome'
-
 DEFAULT_LANG = u'en'
-
 DEFAULT_DATE_FORMAT = ('%d/%b/%Y %H:%M')
 
+# Rel/abs URLs
 RELATIVE_URLS = True
 
+# Control categories display on navigation bar
 HIDE_CATEGORIES_FROM_MENU = True
 
 # URLs formatting
 ARTICLE_URL = "{date:%Y}/{date:%m}/{slug}.html"
 ARTICLE_SAVE_AS = "{date:%Y}/{date:%m}/{slug}.html"
 
-# Sharethis public key
-# SHARETHIS_PUB_KEY = '(575237d0-911a-4d3f-81c4-4cd875623044)'
-
+# Social widget
 DISQUS_SITENAME = "csparpa"
 
-# Feed generation is usually not desired when developing
+# Feed generation
 FEED_ALL_ATOM = 'blog/feeds/all.atom.xml'
 CATEGORY_FEED_ATOM = 'blog/feeds/%s.atom.xml'
 
-# Blogroll
+# Blogroll links
 LINKS = (('Website', 'http://csparpa.tk'),
          ('Time is a gentleman', 'http://claudiosparpaglione.wordpress.com/'),)
 
-# Social widget
+# Social links
 SOCIAL = (('GitHub', 'https://github.com/csparpa'),
           ('Linkedin', 'http://linkedin.com/in/claudiosparpaglione'),
           ('Twitter', '@csparpa'),)
 
-
+# How many article previews per page
 DEFAULT_PAGINATION = 5
 
+# The graphical theme for the blog
 THEME = 'themes/fresh'
--- preview.sh
+++ preview.sh
@@ -8,6 +8,8 @@ mkdir output/blog/img
 cp images/*.png output/blog/img
 echo '***Copying static website to ./output/ folder...'
 cp -R website/* output/
-cd output/
+echo '***Copying CNAME settings to ./output folder...'
+cp extra/CNAME output/
 echo '***Serving on http://localhost:8000...'
+cd output/
 python -m SimpleHTTPServer &> /dev/null
