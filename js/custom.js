40354d9dd08aa6f6c8cf48778a495c445ae9e848
Claudio <csparpa@gmail.com>
2015-03-22 15:12:13 +0100

    Fix minification bug

--- js/custom.js
+++ js/custom.js
@@ -1,4 +1,65 @@
-jQuery.fn.center=function(){this.css("position","absolute");this.css("top",Math.max(0,(($(window).height()-$(this).outerHeight())/2)+
-$(window).scrollTop())+"px");this.css("left",Math.max(0,(($(window).width()-$(this).outerWidth())/2)+
-$(window).scrollLeft())+"px");return this;}
-$(document).ready(function(){ $('#qr_code').on('click',function(){var qr=$('#qr_popup');$(qr).fadeIn('fast');qr.center();var overlay=$('#black_overlay');$(overlay).fadeIn('fast');$(overlay).on('click',function(){$(qr).fadeOut('f');$(this).fadeOut('fast');});}); $('#talks').on('click',function(){var talks=$('#talks_popup');$(talks).fadeIn('fast');talks.center();var overlay=$('#black_overlay');$(overlay).fadeIn('fast');$(overlay).on('click',function(){$(talks).fadeOut('fast');$(this).fadeOut('fast');});}); $('#email').on('click',function(){var email=$('#email_popup');$(email).fadeIn('fast');email.center();var overlay=$('#black_overlay');$(overlay).fadeIn('fast');$(overlay).on('click',function(){$(email).fadeOut('fast');$(this).fadeOut('fast');});}); $('#location').on('click',function(){var location=$('#location_popup');$(location).fadeIn('fast');location.center();var overlay=$('#black_overlay');$(overlay).fadeIn('fast');$(overlay).on('click',function(){$(location).fadeOut('fast');$(this).fadeOut('fast');});});});
+jQuery.fn.center = function () {
+    this.css("position","absolute");
+    this.css("top", Math.max(0, (($(window).height() - $(this).outerHeight()) / 2) + 
+                                                $(window).scrollTop()) + "px");
+    this.css("left", Math.max(0, (($(window).width() - $(this).outerWidth()) / 2) + 
+                                                $(window).scrollLeft()) + "px");
+    return this;
+}
+
+
+$(document).ready(function(){
+
+	//qr link
+	$('#qr_code').on('click', function(){
+		var qr = $('#qr_popup');
+        $(qr).fadeIn('fast');
+        qr.center();
+        var overlay = $('#black_overlay');
+        $(overlay).fadeIn('fast');
+        $(overlay).on('click', function(){
+          $(qr).fadeOut('f');
+          $(this).fadeOut('fast');
+        });
+	});
+
+	//talks link
+	$('#talks').on('click', function(){
+        var talks = $('#talks_popup');
+        $(talks).fadeIn('fast');
+        talks.center();
+        var overlay = $('#black_overlay');
+        $(overlay).fadeIn('fast');
+        $(overlay).on('click', function(){
+          $(talks).fadeOut('fast');
+          $(this).fadeOut('fast');
+        });
+	});
+
+    // email icon
+    $('#email').on('click', function(){
+        var email = $('#email_popup');
+        $(email).fadeIn('fast');
+        email.center();
+        var overlay = $('#black_overlay');
+        $(overlay).fadeIn('fast');
+        $(overlay).on('click', function(){
+          $(email).fadeOut('fast');
+          $(this).fadeOut('fast');
+        });
+    });
+
+
+    // location icon
+    $('#location').on('click', function(){
+        var location = $('#location_popup');
+        $(location).fadeIn('fast');
+        location.center();
+        var overlay = $('#black_overlay');
+        $(overlay).fadeIn('fast');
+        $(overlay).on('click', function(){
+          $(location).fadeOut('fast');
+          $(this).fadeOut('fast');
+        });
+    });
+});
